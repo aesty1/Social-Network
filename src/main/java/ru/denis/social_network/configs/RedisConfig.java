173c3d55f53a0092a23ad8e7bad654afb7ac7a0d
aesty1 <kadirovdenis123@gmail.com>
2025-08-15 13:37:52 +0300

    finish commit

--- src/main/java/ru/denis/social_network/configs/RedisConfig.java
+++ src/main/java/ru/denis/social_network/configs/RedisConfig.java
@@ -1,18 +1,32 @@
 package ru.denis.social_network.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String hostName;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        // ! You have to provide the redisStandaloneConfiguration or else the app wont
+        // ! work with docker
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(hostName);
+        redisStandaloneConfiguration.setPort(port);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
+
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
